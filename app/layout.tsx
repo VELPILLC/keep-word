8fe2ed1ffd0c712cb768d8cf74e30caa747790e1
Angel <bsbagency@velpi.net>
2026-05-15 23:44:19 -0500

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ export const viewport = {
   maximumScale: 1,
   userScalable: false,
 };
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
